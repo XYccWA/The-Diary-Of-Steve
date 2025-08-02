d2684a9787e90d45291758a13538d9f9c933a985
XYccWA <zzb2888988@outlook.com>
2025-08-02 21:49:22 +0800

    techpoint实现，报错未修复

--- src/main/java/com/XYccWA/thediaryofsteve/Event/ForgeEventListener.java
+++ src/main/java/com/XYccWA/thediaryofsteve/Event/ForgeEventListener.java
@@ -6,7 +6,12 @@ import net.minecraft.world.level.block.CropBlock;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+import static com.XYccWA.thediaryofsteve.TheDiaryOfSteve.MODID;
+
+
+@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ForgeEventListener {
 
     @SubscribeEvent
--- src/main/java/com/XYccWA/thediaryofsteve/Event/ModEventListener.java
+++ src/main/java/com/XYccWA/thediaryofsteve/Event/ModEventListener.java
@@ -3,7 +3,12 @@ package com.XYccWA.thediaryofsteve.Event;
 import com.XYccWA.thediaryofsteve.Capability.Technology.PlayerTechPointProvider;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+import static com.XYccWA.thediaryofsteve.TheDiaryOfSteve.MODID;
+
+
+@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ModEventListener {
 
     @SubscribeEvent
