ebb7e4f239067cf922a0eab09895139372474e89
XYccWA <zzb2888988@outlook.com>
2025-08-08 02:27:04 +0800

    添加研究台，模型不显示

---
+++ src/main/java/com/XYccWA/thediaryofsteve/Capability/TechTree/LogicalThinking/PlayerLogicalThinking.java
@@ -0,0 +1,44 @@
+package com.XYccWA.thediaryofsteve.Capability.TechTree.LogicalThinking;
+
+import net.minecraft.nbt.CompoundTag;
+
+public class PlayerLogicalThinking {
+    private double logicalThinking;
+
+    public PlayerLogicalThinking() { this.logicalThinking = 0.8; }
+
+    public void increace(){
+        logicalThinking++;
+    }
+
+    public void increace(double i){
+        logicalThinking += i;
+    }
+
+    public void decreace(double i){
+        if (logicalThinking >= i)
+            logicalThinking -= i;
+        else
+            logicalThinking = 0;
+    }
+
+    public void decreace(){
+        this.decreace(0.01);
+    }
+
+    public double getLogicalThinking() {
+        return logicalThinking;
+    }
+
+    public void setLogicalThinking(double logicalThinking) {
+        this.logicalThinking = logicalThinking;
+    }
+
+    public void saveNBTData(CompoundTag compoundTag){
+        compoundTag.putDouble("logicalthinking", logicalThinking);
+    }
+
+    public void loadNBTData(CompoundTag compoundTag){
+        logicalThinking = compoundTag.getDouble("logicalthinking");
+    }
+}
---
+++ src/main/java/com/XYccWA/thediaryofsteve/Capability/TechTree/LogicalThinking/PlayerLogicalThinkingProvider.java
@@ -0,0 +1,51 @@
+package com.XYccWA.thediaryofsteve.Capability.TechTree.LogicalThinking;
+
+
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.common.capabilities.CapabilityToken;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.INBTSerializable;
+import net.minecraftforge.common.util.LazyOptional;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class PlayerLogicalThinkingProvider implements ICapabilityProvider, INBTSerializable {
+    private PlayerLogicalThinking playerLogicalThinking;
+    public static final Capability<PlayerLogicalThinking> PLAYER_LOGICAL_THINKING_CAPABILITY = CapabilityManager.get(new CapabilityToken<PlayerLogicalThinking>() {});
+    private final LazyOptional<PlayerLogicalThinking> lazyOptional = LazyOptional.of(() -> this.playerLogicalThinking);
+
+
+    public PlayerLogicalThinkingProvider(){
+        this.playerLogicalThinking = new PlayerLogicalThinking();
+    }
+
+    @Override
+    public Tag serializeNBT() {
+        var tag = new CompoundTag();
+        playerLogicalThinking.saveNBTData(tag);
+        return tag;
+    }
+
+    @Override
+    public void deserializeNBT(Tag nbt) {
+        playerLogicalThinking.loadNBTData((CompoundTag) nbt);
+    }
+
+    @Override
+    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        return getCapability(cap);
+    }
+
+    @Override
+    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap) {
+        if(cap == PLAYER_LOGICAL_THINKING_CAPABILITY){
+            return lazyOptional.cast();
+        }else{
+            return lazyOptional.empty();
+        }
+    }
+}
